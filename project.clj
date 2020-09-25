b9b3de22cc1e5af957e26c953ea1d7264787269c
Daniel Glauser <daniel@inspirefitness.com>
2020-09-25 10:05:25 -0600

    The latest version of Clojure and added in Malli/Retit

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject mozen "0.1.0-SNAPSHOT"
   :description "A framework for balancing workloads across available hardware."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/danielglauser/mozen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [metosin/reitit "0.5.5"]])
